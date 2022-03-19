8d81d3f06b7d795eed3ca2038381ed7de68abb1b
Texnoservis <Texnoservis@DESKTOP-21ME2E7>
2022-03-19 13:38:53 +0400

    git commit

--- MyProject/src/main/java/MelumatAl.java
+++ MyProject/src/main/java/MelumatAl.java
@@ -15,14 +15,15 @@ import java.sql.Statement;
  *
  * @author Texnoservis
  */
-public class MelumatAl {
-    Musteri melumatAl(String ss) throws SQLException{
+public  class MelumatAl {
+    static Musteri melumatAl(String ss) throws SQLException{
     Musteri mstr=new Musteri();
     
     Connection cnt=DriverManager.getConnection("jdbc:mysql://localhost/my_first_db","root","Mamedov1759");
      PreparedStatement pstmt=cnt.prepareStatement("select * from musteri left join olke on olke.id=musteri.id where musteri.ad=?");
      pstmt.setString(1, ss);
     ResultSet rs=pstmt.executeQuery();
+        System.out.println("Connc ok");
     while(rs.next()){
         mstr.setAd(rs.getString("ad"));
         mstr.setId(rs.getInt("id"));
--- MyProject/src/main/java/Test.java
+++ MyProject/src/main/java/Test.java
@@ -3,9 +3,9 @@
 public class Test{
     public static void main(String[] args) throws Exception {
        
-        MelumatAl ma=new MelumatAl();
-        Musteri mst=ma.melumatAl("Cavid");
-        System.out.println(mst.toString());
+        
+        Musteri mst=MelumatAl.melumatAl("Cavid");
+        System.out.println(mst);
    
         
     }
